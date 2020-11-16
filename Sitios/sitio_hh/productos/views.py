ff07cf988fe87d95c60c0fec87dbc206c03faadb
SetHet <36393893+SetHet@users.noreply.github.com>
2020-11-16 17:38:19 -0300

    sitio_hh, preparando filtro busqueda

--- Sitios/sitio_hh/productos/views.py
+++ Sitios/sitio_hh/productos/views.py
@@ -4,7 +4,42 @@ from .models import Categoria, Producto
 # Create your views here.
 def search(request):
 
-    list_categorias = Categoria.objects.all()
-    list_productos = Producto.objects.all()
+    if (request.method == 'GET'):
+
+        search = request.GET
+        print(search)
+
+        groups = search['search'].split(' ')
+        print(groups)
+
+        categoriaFiltro = ""
+        wordsFiltro = []
+
+        for grp in groups:
+            comandos = grp.split(':')
+            if len(comandos) > 1:
+                print(comandos[0])
+                if comandos[0].lower() == "categoria":
+                    categoriaFiltro = comandos[1]
+                    continue
+            wordsFiltro.append(comandos[0])
+
+        print("cat > " + categoriaFiltro)
+        print("words > " + str(wordsFiltro))
+
+        query = Producto.objects.all()
+        if categoriaFiltro != "":
+            query = query.filter(categoria__iexact=categoriaFiltro)
+        
+        query_original = query
+
+        for word in wordsFiltro:
+            query = query | query_original.filter()
+
+        list_categorias = Categoria.objects.all()
+        list_productos = query
+    else:
+        list_categorias = Categoria.objects.all()
+        list_productos = Producto.objects.all()
 
     return render(request, "productos/search.html", {'list_categorias':list_categorias, 'list_productos':list_productos})  

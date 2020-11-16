771818b0066e28daa269b4aebaa1a56c4f05f219
SetHet <36393893+SetHet@users.noreply.github.com>
2020-11-16 19:35:45 -0300

    Filtro de productos listo

--- Sitios/sitio_hh/productos/views.py
+++ Sitios/sitio_hh/productos/views.py
@@ -15,26 +15,30 @@ def search(request):
         categoriaFiltro = ""
         wordsFiltro = []
 
-        for grp in groups:
-            comandos = grp.split(':')
-            if len(comandos) > 1:
-                print(comandos[0])
-                if comandos[0].lower() == "categoria":
-                    categoriaFiltro = comandos[1]
-                    continue
-            wordsFiltro.append(comandos[0])
+        if not (len(groups) == 1 and groups[0] == ""):
+            for grp in groups:
+                comandos = grp.split(':')
+                if len(comandos) > 1:
+                    print(comandos[0])
+                    if comandos[0].lower() == "categoria":
+                        categoriaFiltro = comandos[1]
+                        continue
+                wordsFiltro.append(comandos[0])
 
         print("cat > " + categoriaFiltro)
         print("words > " + str(wordsFiltro))
 
         query = Producto.objects.all()
         if categoriaFiltro != "":
-            query = query.filter(categoria__iexact=categoriaFiltro)
+            query = query.filter(categoria__title__iexact=categoriaFiltro)
         
-        query_original = query
+        if len(wordsFiltro) > 0:
+            query_original = query
+            query = query_original.filter(name__icontains=wordsFiltro[0])
+            for word in wordsFiltro:
+                query = query | query_original.filter(name__icontains=word)
+            
 
-        for word in wordsFiltro:
-            query = query | query_original.filter()
 
         list_categorias = Categoria.objects.all()
         list_productos = query

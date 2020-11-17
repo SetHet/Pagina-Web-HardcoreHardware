dec6f4e4888a73f596980b0ef56979ff25cadbf7
SetHet <36393893+SetHet@users.noreply.github.com>
2020-11-17 15:32:24 -0300

    Update views.py
    Modularizacion 1: Filtro

--- Sitios/prueba_django/products/views.py
+++ Sitios/prueba_django/products/views.py
@@ -38,20 +38,8 @@ def search(request):
                 # Si no es un filtro complejo se guarda en palabras filtro
                 wordsFiltro.append(valor)
 
-        # Se comienza con un 'select * from producto;'
-        query = Producto.objects.all()
-
-        # Si existe filtro de categoria especifico se filtra para que solo pertenescan a esa categoria
-        if categoriaFiltro != "":
-            query = query.filter(categoria__title__iexact=categoriaFiltro)
-        
-        # Si hay palabras filtro se buscan todos los productos que contengan alguna de las palabras
-        if len(wordsFiltro) > 0:
-            query_original = query
-            # primer filtro para tener una base
-            query = query_original.filter(name__icontains=wordsFiltro[0]) 
-            for word in wordsFiltro:
-                query = query | query_original.filter(Q(name__icontains=word) | Q(descripcion__icontains=word) | Q(categoria__title__icontains=word))
+        # Se filtra la busqueda en la BD
+        query = BusquedaFiltrada(wordsFiltro, categoriaFiltro)
 
         # Se rescatan las categorias y productos seleccionados para enviarlos
         list_categorias = Categoria.objects.all()
@@ -60,6 +48,24 @@ def search(request):
         # Se rescatan todas las categorias y productos para enviarlos si no hay filtro
         list_categorias = Categoria.objects.all()
         list_productos = Producto.objects.all()
-
     
     return render(request, "products/search.html", {'list_categorias':list_categorias, 'list_productos':list_productos})  
+
+
+def BusquedaFiltrada(wordsFiltro, categoriaFiltro):
+    # Se comienza con un 'select * from producto;'
+    query = Producto.objects.all()
+
+    # Si existe filtro de categoria especifico se filtra para que solo pertenescan a esa categoria
+    if categoriaFiltro != "":
+        query = query.filter(categoria__title__iexact=categoriaFiltro)
+    
+    # Si hay palabras filtro se buscan todos los productos que contengan alguna de las palabras
+    if len(wordsFiltro) > 0:
+        query_original = query
+        # primer filtro para tener una base
+        query = query_original.filter(name__icontains=wordsFiltro[0]) 
+        for word in wordsFiltro:
+            query = query | query_original.filter(Q(name__icontains=word) | Q(descripcion__icontains=word) | Q(categoria__title__icontains=word))
+
+    return query

c44bf0fb7f59395c71a9160bc25cf7b6744a448e
Benjamin Arias <63750721+KaltGeist@users.noreply.github.com>
2020-12-07 15:25:44 -0300

    Revert "Update views.py"
    This reverts commit 65e4d6bf4413e983a20db764c70217d41ef34ccf.

--- Sitios/prueba_django/register/views.py
+++ Sitios/prueba_django/register/views.py
@@ -1,16 +1,3 @@
 from django.shortcuts import render
-from .models import Registro
+
 # Create your views here.
-def register(request):
-    if request.method == "POST":
-        rut = request.POST['rut']
-        name = request.POST['name']
-        username = request.POST['username']
-        birthdate = request.POST['birthdate']
-        address = request.POST['address']
-        email = request.POST['email']
-        password = request.POST['password']
-        comments = request.POST['comments']
-        user = Registro(rut=rut, name=name, username=username, birthdate=birthdate, address=address, email=email, password=password, comments=comments)
-        user.save()
-    return render(request, "register/register.html")

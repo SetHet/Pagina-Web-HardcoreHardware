410fb93ea18422499f6342a5537bf6a22ad7f999
Harasho <65163071+iharasho@users.noreply.github.com>
2020-12-07 15:27:25 -0400

    Revert "Merge branch 'Backend' of https://github.com/DWY-005D-2020-2-SunLightWarriors/DWY4101-Eval2-HardcoreHardware into Backend"
    This reverts commit b93e8d78ab925aeb9d46581225cca0d44b54e17f, reversing
    changes made to 572f93722f0131ef59e2e59f392a1090888ae8af.

--- Sitios/prueba_django/core/views.py
+++ Sitios/prueba_django/core/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from products import views as products_views
-from products.models import Categoria, Producto
-import random
+from products.models import Categoria
 
 # Create your views here.
 def home(request):
-
-    dictionary_models = ProductosCarruselHome(baseRequired())
-
-    return products_views.home(request, dictionary_models)
+    return products_views.home(request, baseRequired())
 
 
 def guias(request):
@@ -32,58 +28,3 @@ def baseRequired():
     }
 
     return dicctionary
-
-
-
-# Funciones
-
-def ProductosCarruselHome(dictionary):
-
-    cantidad_final = 16
-    seleccionados_index = []
-    all_products = Producto.objects.all()
-
-    for ciclo in range(cantidad_final):
-        #Encontrar uno random
-        pos = random.randint(0, len(all_products) - 1)
-        pos_random = pos
-        if not pos in seleccionados_index:
-            seleccionados_index.append(pos)
-            continue
-        #Buscar en el resto de la lista
-        pos+=1
-        while pos % len(all_products) != pos_random:
-            new_pos = pos % len(all_products)
-            if not new_pos in seleccionados_index:
-                seleccionados_index.append(new_pos)
-                break
-            pos+=1
-        if pos % len(all_products) != pos_random:
-            continue
-        #Tomar valor random aunque este repetido
-        seleccionados_index.append(pos_random)
-
-    #Almacenar productos seleccionados
-    selected_products = []
-    for index in seleccionados_index:
-        selected_products.append(all_products[index])
-
-    #Repartir
-    list_productos_carrousel_1 = selected_products[0:4]
-    list_productos_carrousel_2_1 = selected_products[4:7]
-    list_productos_carrousel_2_2 = selected_products[7:10]
-    list_productos_carrousel_3_1 = selected_products[10:13]
-    list_productos_carrousel_3_2 = selected_products[13:16]
-
-    dictionary["list_productos_carrousel_1"] = list_productos_carrousel_1
-    dictionary["list_productos_carrousel_2_1"] = list_productos_carrousel_2_1
-    dictionary["list_productos_carrousel_2_2"] = list_productos_carrousel_2_2
-    dictionary["list_productos_carrousel_3_1"] = list_productos_carrousel_3_1
-    dictionary["list_productos_carrousel_3_2"] = list_productos_carrousel_3_2
-
-    return dictionary
-        
-
-
-
-
